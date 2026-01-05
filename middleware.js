8058367e9247aa2e292abe4f2f5970dd531312b0
KelvinProgrammer <mwangikelvin3@gmail.com>
2026-01-05 11:35:05 +0300

    Updated System

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
 
 // Use edge-compatible auth config (no Node.js crypto dependencies)
-export const { auth: middleware } = NextAuth(authConfig)
+export default NextAuth(authConfig).auth
 
 export const config = {
   matcher: ["/dashboard/:path*", "/login", "/signup"],
